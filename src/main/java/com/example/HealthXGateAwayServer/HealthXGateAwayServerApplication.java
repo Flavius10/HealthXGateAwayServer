07405f56aaf6f5eb34f9b68b6514e7b38d8e9d4a
Flavius10 <flaviusa1010@gmail.com>
2025-10-16 11:56:14 +0300

    Start the project

---
+++ src/main/java/com/example/HealthXGateAwayServer/HealthXGateAwayServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.HealthXGateAwayServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthXGateAwayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthXGateAwayServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HealthXGateAwayServer/HealthXGateAwayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HealthXGateAwayServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthXGateAwayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
