97039f33175bb261b3db4b117c34e5c0b4477b48
Flavius10 <flaviusa1010@gmail.com>
2025-10-16 12:24:34 +0300

    Implement the GateAwayConfig

--- src/main/java/com/example/HealthXGateAwayServer/config/GateAwayConfig.java
+++ src/main/java/com/example/HealthXGateAwayServer/config/GateAwayConfig.java
@@ -1,4 +1,4 @@
-package config;
+package com.example.HealthXGateAwayServer.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
